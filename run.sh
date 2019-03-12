849ce0983bba8518a4a82031863a75cc1bd53605
Vincenzo Rubano <vincenzorubano@email.it>
2019-03-12 13:19:39 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 sbt --error assembly &&
 spark-submit \
   --class "Main" \
-  target/scala-2.11/scalable-hashtag-recommender-system-assembly-0.1.jar "$@"
+--driver-memory 2g \
+--executor-cores 6 \
+--executor-memory 10g \
+  ./target/scala-2.11/scalable-hashtag-recommender-system-assembly-0.1.jar \
+--features-file /Users/vincenzo/Downloads/pca130.csv --tag-file /Users/vincenzo/iaproject/dataset/tag_listc.txt --image-path "https://image.ibb.co/kYdbKT/IMG_20180725_194058_490.jpg" --out-cluster-file clusters.csv
+
