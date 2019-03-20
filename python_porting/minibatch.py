07b1fbd488da5a879dfa457472dfd87cf1df6a1b
Vincenzo Rubano <vincenzorubano@email.it>
2019-03-20 11:27:28 +0100

    Add wss suppot

--- python_porting/minibatch.py
+++ python_porting/minibatch.py
@@ -11,11 +11,21 @@ import pickle
 path = "./data/pca_130_new.csv"
 arr = pd.read_csv(path, sep=",", header=None, index_col=None, dtype="float16")
 arr = arr.to_numpy()
-k = 169
-mbk = MiniBatchKMeans(init='k-means++', n_clusters=k, batch_size=100,
+
+ks = {}
+lastMinwss = 3420000
+
+for k in range(21, 171):
+  mbk = MiniBatchKMeans(init='k-means++', n_clusters=k, batch_size=100,
                       n_init=3, max_no_improvement=10, verbose=0)
-mbk.fit(arr)
-print(len(mbk.cluster_centers_))
-print(np.argwhere(np.isnan(mbk.cluster_centers_)))
-print(silhouette_score(arr, mbk.labels_, metric="euclidean",
-                                   sample_size=None, random_state=None))
+  mbk.fit(arr)
+  minwss = mbk.inertia_
+  print(minwss)
+if minwss < lastMinwss:
+    lastMinwss = minwss
+    ks['wss'] = k
+#print(ks)
+      #for (method, k) in ks:
+#print(f'For {method} k is {k}')
+
+  #print(silhouette_score(arr, mbk.labels_, metric="euclidean", sample_size=None, random_state=None))
