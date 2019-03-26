6c5bfe89ede673904af487dfcb286e29aa885305
Vincenzo Rubano <vincenzorubano@email.it>
2019-03-26 18:52:29 +0100

    Add script for converting file to pickle

---
+++ datapickle.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+import os
+import numpy as np
+import pickle
+
+
+path = sys.argv[1]
+serial_path = os.path.splitext(path)[0] + '.pickle'
+arr = pd.read_csv(path, sep=",", header=None, index_col=None, dtype="float16")
+print(arr.shape)
+pickle.dump(arr, open(serial_path, 'wb'))
